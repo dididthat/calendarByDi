e4f55ff846a01786689c50df1f52dbcc588c0690
dididthat <darianikulina02@gmail.com>
2023-04-24 17:37:08 +0300

    done_

--- CalendarByDi/Controllers/DescriptionViewController.swift
+++ CalendarByDi/Controllers/DescriptionViewController.swift
@@ -8,48 +8,108 @@
 import UIKit
 
 final class DescriptionViewConrtoller: UIViewController {
+    
+    
     var toDo: ToDoModel?
-//    private let dayTimeLabel: UILabel = {
-//        let label = UILabel()
-//        label.textColor = .black
-//        label.font = UIFont(name: "Avenir Next Demi Bold", size: 20)
-//        label.textAlignment = .left
-//        label.adjustsFontSizeToFitWidth = true
-//        label.translatesAutoresizingMaskIntoConstraints = false
-//        return label
-//    }()
-//    
-//    private let toDoLabel: UILabel = {
-//        let label = UILabel()
-//        label.textColor = .black
-//        label.font = UIFont(name: "Avenir Next", size: 20)
-//        label.textAlignment = .right
-//        label.adjustsFontSizeToFitWidth = true
-//        label.translatesAutoresizingMaskIntoConstraints = false
-//        return label
-//    }()
-//    
+    
+    private let nameToDoLabel: UILabel = {
+        let label = UILabel()
+        
+        label.textColor = .black
+        label.font = UIFont(name: "Arial Rounded MT Bold", size: 20)
+        label.textAlignment = .left
+        label.adjustsFontSizeToFitWidth = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        return label
+    }()
+    
+    private let dateToDoLabel: UILabel = {
+        let label = UILabel()
+        
+        label.textColor = .black
+        label.font = UIFont(name: "Avenir Next", size: 20)
+        label.textAlignment = .center
+        label.adjustsFontSizeToFitWidth = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        return label
+    }()
+    
+    private let timeToDoLabel: UILabel = {
+        let label = UILabel()
+        
+        label.textColor = .black
+        label.font = UIFont(name: "Avenir Next", size: 20)
+        label.textAlignment = .center
+        label.adjustsFontSizeToFitWidth = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        return label
+    }()
+    
+    private let descriptionToDoLabel: UILabel = {
+        let label = UILabel()
+        
+        label.textColor = .black
+        label.font = UIFont(name: "Avenir Next", size: 20)
+        label.textAlignment = .center
+        label.adjustsFontSizeToFitWidth = true
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if let date = toDo?.getDateStart() {
+            let calendar = Calendar.current
+            let hour = calendar.component(.hour, from: date)
+            let minutes = calendar.component(.minute, from: date)
+            let day = calendar.component(.day, from: date)
+            let month = calendar.component(.month, from: date)
+            let year  = calendar.component(.year, from: date)
+            timeToDoLabel.text = "\(hour):\(minutes)"
+            dateToDoLabel.text = "\(day).\(month).\(year)"
+        }
+        
+        nameToDoLabel.text = toDo?.name
+        descriptionToDoLabel.text = toDo?.description
+        
         view.backgroundColor = .white
         title = "Description"
         
+        setupView()
     }
-//    private func setConstraints() {
-//        let stackView = UIStackView()
-//        stackView.addArrangedSubview(dayTimeLabel)
-//        stackView.addArrangedSubview(toDoLabel)
-//        stackView.axis = .horizontal
-//        stackView.spacing = 10
-//        stackView.distribution = .fillEqually
-//        stackView.translatesAutoresizingMaskIntoConstraints = false
-//        
-//        self.addSubview(stackView)
-//        NSLayoutConstraint.activate([
-//            stackView.topAnchor.constraint(equalTo: self.topAnchor, constant: 10),
-//            stackView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 5),
-//            stackView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -5),
-//            stackView.heightAnchor.constraint(equalToConstant: 25)
-//        ])
-//    }
+    
+    private func setupView() {
+        let stackView = UIStackView()
+        
+        stackView.addArrangedSubview(nameToDoLabel)
+        stackView.addArrangedSubview(dateToDoLabel)
+        stackView.addArrangedSubview(timeToDoLabel)
+        stackView.addArrangedSubview(descriptionToDoLabel)
+        stackView.addArrangedSubview(UIView())
+        
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.distribution = .fill
+        stackView.alignment = .top
+        
+        
+        view.addSubview(stackView)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor,constant: -50).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor,constant: -30).isActive = true
+        
+    }
 }
